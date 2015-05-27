1ded639497b5ba30eb99618432551ba2670edd92
BC Broussard <bc.broussard@flyingcranes.com>
2015-05-27 15:00:11 -0700

    make accompanying binaries runnable by anyone

--- kubernetes/master/scripts/rpm/after-install.sh
+++ kubernetes/master/scripts/rpm/after-install.sh
@@ -5,6 +5,10 @@ if [ $1 -eq 1 ] ; then
         chmod +x /usr/bin/kube-controller-manager
         chmod +x /usr/bin/kubelet
 
+        chmod +x /usr/bin/kubectl
+        chmod +x /usr/bin/hyperkube
+        chmod +x /usr/bin/kubernetes
+
         # Initial installation
-        systemctl preset kube-apiserver kube-scheduler kube-controller-manager kubelet kube-proxy >/dev/null 2>&1 || :
+        systemctl preset kube-apiserver kube-scheduler kube-controller-manager >/dev/null 2>&1 || :
 fi
